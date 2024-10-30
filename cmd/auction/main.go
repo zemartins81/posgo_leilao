2f32d4c9f0cc5bfdfcc61dad4f286583f60e34d1
José C. Martins <zemartins81@gmail.com>
2024-10-29 22:01:38 -0300

    Add MongoDB connection setup
    Introduced MongoDB connection initialization in the main function, leveraging a new database connection method. Included error handling and context management. A new `.env` file now holds MongoDB connection configurations.

--- cmd/auction/main.go
+++ cmd/auction/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"context"
+	"github.com/zemartins81/posgo_leilao/configuration/database/mongodb"
 	"log"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-    if err := godotenv.Load() ; err != nil {
-        log.Fatal("Error loading .env file")
-        return
-    }
+	ctx := context.Background()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
+
+	databaseClient, err := mongodb.NewMongoDBConnections(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
 }
--- configuration/database/mongodb/connection.go
+++ configuration/database/mongodb/connection.go
@@ -1,10 +1,32 @@
 package mongodb
 
-const (
-MONGODB_URL = "MONGODB_URL"
-MONGODB_DB  = "MONGODB_DB"
+import (
+	"context"
+	"github.com/zemartins81/posgo_leilao/configuration/logger"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 )
 
-func NewMongoDBConnections() {
+const (
+	MONGODB_URL = "MONGODB_URL"
+	MONGODB_DB  = "MONGODB_DB"
+)
 
+func NewMongoDBConnections(ctx context.Context) (*mongo.Database, error) {
+	mongoUrl := os.Getenv(MONGODB_URL)
+	mongoDatabase := os.Getenv(MONGODB_DB)
 
+	client, err := mongo.Connect(
+		ctx, options.Client().ApplyURI(mongoUrl))
+	if err != nil {
+		logger.Error("Error trying to connect to MongoDB: ", err)
+		return nil, err
+	}
+	if err := client.Ping(ctx, nil); err != nil {
+		logger.Error("Error trying to ping MongoDB: ", err)
+		return nil, err
+	}
+
+	return client.Database(mongoDatabase), nil
+}
